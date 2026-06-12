3b30d56e65bb59927d376055cddb108d8820407e
juliamelnikova57-ctrl <juliamelnikova57@gmail.com>
2026-06-12 22:25:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,7 @@ app.post('/api/chat', (req, res) => {
   r.write(body);
   r.end();
 });
-
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
